23437f379a8c647546b2f9d715fe06805ce07d0b
peter <apppeterpan@gmail.com>
2023-12-13 20:00:58 +0800

    Initial Commit

---
+++ Xmas/XmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  XmasApp.swift
+//  Xmas
+//
+//  Created by Peter Pan on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct XmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
